2bfb0d9addea670c2b9895ca6a0d4b00cf5b0b81
Fachidiot <54926396+Fachidiot@users.noreply.github.com>
2026-01-28 00:50:16 +0900

    # worm Animation

--- Assets/01. Scripts/03. Core/Animal/StateMachine/BirdStateMachine.cs
+++ Assets/01. Scripts/03. Core/Animal/StateMachine/BirdStateMachine.cs
@@ -1,32 +1,24 @@
 using AIStates;
 using BirdStates;
-using BreedStates;
 using UnityEngine;
 
-public class BirdStateMachine : AnimalStateMachine, IBird, IBreedable
+public class BirdStateMachine : AnimalStateMachine
 {
     [SerializeField] private bool isFlying;
 
     private readonly Idle idleState = new Idle();
-    private readonly Patrol patrolState = new Patrol();
-    private readonly Trace traceState = new Trace();
-    private readonly Interact interactState = new Interact();
-    private readonly Attack attackState = new Attack();
+    private readonly FlyPatrol flyPatrol = new FlyPatrol();
+    private readonly FlyStalking flyStalking = new FlyStalking();
+    private readonly FlyDive flyDive = new FlyDive();
+    private readonly FlyAscent flyAscent = new FlyAscent();
     private readonly Hit hitState = new Hit();
     private readonly Die dieState = new Die();
+
     public override BaseState<AIController> IdleState => idleState;
-    public override BaseState<AIController> PatrolState => patrolState;
-    public override BaseState<AIController> TraceState => traceState;
-    public override BaseState<AIController> InteractState => interactState;
-    public override BaseState<AIController> AttackState => attackState;
+    public override BaseState<AIController> PatrolState => flyPatrol;
+    public BaseState<AIController> StalkingState => flyStalking;
+    public BaseState<AIController> DiveState => flyDive;
+    public BaseState<AIController> AscentState => flyAscent;
     public override BaseState<AIController> HitState => hitState;
     public override BaseState<AIController> DieState => dieState;
-
-    private readonly Fly flyState = new Fly();
-    public BaseState<AIController> FlyState => flyState;
-
-    private readonly Breed breedState = new Breed();
-    private readonly Feed feedState = new Feed();
-    public BaseState<AIController> BreedState => breedState;
-    public BaseState<AIController> FeedState => feedState;
 }
--- Assets/01. Scripts/03. Core/Animal/StateMachine/States/BirdStates.cs
+++ Assets/01. Scripts/03. Core/Animal/StateMachine/States/BirdStates.cs
@@ -19,4 +19,94 @@ namespace BirdStates
             throw new System.NotImplementedException();
         }
     }
+
+    public class FlyPatrol : BaseState<AIController>
+    {
+        public override void EnterState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ExitState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override BaseState<AIController> UpdateState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class FlyStalking : BaseState<AIController>
+    {
+        public override void EnterState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ExitState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override BaseState<AIController> UpdateState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class FlyDive : BaseState<AIController>
+    {
+        public override void EnterState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ExitState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override BaseState<AIController> UpdateState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class FlyAscent : BaseState<AIController>
+    {
+        public override void EnterState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ExitState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override BaseState<AIController> UpdateState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Retreat : BaseState<AIController>
+    {
+        public override void EnterState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ExitState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override BaseState<AIController> UpdateState(AIController animal)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
